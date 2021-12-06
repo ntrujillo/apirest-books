6e7dca350550ee1a541a70b7227dd0d8e62c2be5
Wladimir Trujillo <wladimir.trujillo.ec@gmail.com>
2021-12-05 20:46:01 -0500

    cors

--- src/main/java/com/company/books/backend/controller/BookRestController.java
+++ src/main/java/com/company/books/backend/controller/BookRestController.java
@@ -5,6 +5,7 @@ import com.company.books.backend.response.BookResponseRest;
 import com.company.books.backend.service.BookService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -14,6 +15,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@CrossOrigin(origins = {"http://localhost:4200"})
 @RestController
 @RequestMapping("/v1")
 public class BookRestController {
